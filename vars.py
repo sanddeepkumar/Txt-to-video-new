60904315c74fc657583389f456c06b65cda1f3d4
Sandeepa pattharia <sanju.kaminey@gmail.com>
2025-01-27 21:59:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,7 +2,7 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "26148784)
+API_ID    = os.environ.get("API_ID", "26148784")
 API_HASH  = os.environ.get("API_HASH", "e1428799c0aeccd0a3cd1f15606e7a80")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
